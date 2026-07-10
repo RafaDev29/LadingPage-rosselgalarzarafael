5b5512858911bdc851ccef2ba76dc2cbc4273d52
RafaDev29 <rosselgalarzarafael@gmail.com>
2026-07-09 20:39:54 -0500

    feat: configure Next.js for static export and enable unoptimized images

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true, // Requerido por Next.js para exportaciones estáticas si usas el componente <Image />
+  },
 };
 
 export default nextConfig;
